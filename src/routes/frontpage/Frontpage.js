3132b8c1a0153e1e8479c29e4039c10aa58ac84a
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-09-20 21:23:27 +0000

    Fix frontage parties

--- src/routes/frontpage/Frontpage.js
+++ src/routes/frontpage/Frontpage.js
@@ -7,25 +7,31 @@ import s from './Frontpage.scss';
 class FrontPage extends PureComponent {
   render() {
     return (
-      <div className={s.parties}>
+      <section className={s.parties}>
         {parties.map(party => (
           <a
             href={`/flokkur/${party.url}`}
             className={s.party}
             key={party.letter}
           >
-            <h3 className={s.letter}>
-              <span>x</span>
-              {party.letter}
-            </h3>
-            <img
-              src={`https://s3.eu-west-2.amazonaws.com/assets.kjosturett.is/${party.url}.png`}
-              className={s.image}
-            />
-            <h4 className={s.name}>{party.name}</h4>
+            <span className={s.imgWrap}>
+              <img
+                src={`https://s3.eu-west-2.amazonaws.com/assets.kjosturett.is/${party.url}.png`}
+                className={s.image}
+              />
+            </span>
+            <span className={s.info}>
+              <h3 className={s.name}>{party.name}</h3>
+              <p className={s.leader}>{party.leader}</p>
+
+              <span className={s.letter}>
+                <span>x</span>
+                {party.letter}
+              </span>
+            </span>
           </a>
         ))}
-      </div>
+      </section>
     );
   }
 }
