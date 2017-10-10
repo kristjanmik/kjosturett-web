4ce5c54087116d23033cc51c3a05e4bc1b4b505d
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-10 14:51:31 +0000

    Reusable collapsales

---
+++ src/components/Collapsable/Collapsable.js
@@ -0,0 +1,80 @@
+// @flow
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Scroll from 'react-scroll';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import { Collapse } from 'react-collapse';
+import styles from './Collapsable.scss';
+
+class Collapsable extends React.Component {
+  static propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        image: PropTypes.string,
+      })
+    ),
+  };
+  static defaultProps = {
+    items: [],
+  };
+  static spring = {
+    stiffness: 200,
+    damping: 20,
+  };
+  state = {
+    open: {},
+  };
+  toggle = (key: string) => e => {
+    this.setState(state => {
+      const open = { ...state.open };
+      open[key] = !Boolean(open[key]);
+      return { open };
+    });
+
+    let curtop = 0;
+    if (e.target.offsetParent) {
+      do {
+        curtop += e.target.offsetTop;
+      } while ((e.target = e.target.offsetParent));
+    }
+    Scroll.animateScroll.scrollTo(curtop - 20);
+  };
+  render() {
+    const { items } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={styles.root}>
+        {items.map(({ key, title, content, image }) => (
+          <div className={styles.category} key={key}>
+            <div className={styles.header} onClick={this.toggle(key)}>
+              {image && <img className={styles.image} src={image} />}
+              <h3 className={styles.title}>{title}</h3>
+            </div>
+            <Collapse
+              isOpened={open[key] === true}
+              springConfig={Collapsable.spring}
+            >
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: content,
+                }}
+                className={cx(
+                  styles.content
+                  // category.statement === '' ? s.textNoReply : null
+                )}
+              />
+            </Collapse>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Collapsable);
--- src/routes/partysingle/PartySingle.js
+++ src/routes/partysingle/PartySingle.js
@@ -2,68 +2,29 @@
 
 import React, { PureComponent } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import cx from 'classnames';
-import { Collapse } from 'react-collapse';
-import Scroll from 'react-scroll';
+import Collapsable from '../../components/Collapsable';
 import PartyProfile from '../../components/PartyProfile';
 import s from './PartySingle.scss';
 
-const scroll = Scroll.animateScroll;
-
 class PartySingle extends PureComponent {
-  state = {
-    open: {}
-  };
-  toggleCategory = (category: string) => e => {
-    this.setState(state => {
-      const open = { ...state.open };
-      open[category] = !Boolean(open[category]);
-      return { open };
-    });
-
-    let curtop = 0;
-    if (e.target.offsetParent) {
-      do {
-        curtop += e.target.offsetTop;
-      } while ((e.target = e.target.offsetParent));
-    }
-    scroll.scrollTo(curtop - 20);
-  };
   render() {
-    const { open } = this.state;
     const { party, categories } = this.props;
 
     return (
       <div className={s.root}>
         <PartyProfile {...party} />
-        <div className={s.categories}>
-          {categories.map(category => (
-            <div className={s.category} key={category.category}>
-              <div
-                className={s.info}
-                onClick={this.toggleCategory(category.category)}
-              >
-                <h3 className={s.name}>{category.name}</h3>
-              </div>
-              <Collapse
-                isOpened={open[category.category] === true}
-                springConfig={{ stiffness: 200, damping: 20 }}
-              >
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html:
-                      category.statement ||
-                      `${party.name} hefur ekki skilað inn umfjöllun um ${category.name.toLowerCase()}.`
-                  }}
-                  className={cx(
-                    s.text,
-                    category.statement === '' ? s.textNoReply : null
-                  )}
-                />
-              </Collapse>
-            </div>
-          ))}
-        </div>
+        <Collapsable
+          items={
+            categories &&
+            categories.map(({ name, category, statement }) => ({
+              key: category,
+              title: name,
+              content:
+                statement ||
+                `${party.name} hefur ekki skilað inn umfjöllun um ${name.toLowerCase()}.`,
+            }))
+          }
+        />
       </div>
     );
   }
