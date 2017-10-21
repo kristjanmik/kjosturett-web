584c8bc03d9fbd06b79cd3b97ba3aad1d225af0c
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-21 13:41:47 +0000

    Move answers map + defaults to a shared location

--- src/routes/prof/KosningaProf.js
+++ src/routes/prof/KosningaProf.js
@@ -6,28 +6,24 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { encodeAnswersToken } from '../../utils';
 import s from './KosningaProf.scss';
 
-const answerMap = {
-  1: 'Mjög ósammála',
-  2: 'Frekar ósammála',
-  3: 'Hlutlaus',
-  4: 'Frekar sammála',
-  5: 'Mjög sammála'
-};
 const areYouSure =
   'Ertu viss um að þú viljir yfirgefa síðuna núna? Öll svörin munu týnast.';
-const defaultAnswer = '3';
 
 class Kosningaprof extends PureComponent {
   static contextTypes = {
-    fetch: PropTypes.func.isRequired
+    fetch: PropTypes.func.isRequired,
   };
   static propTypes = {
+    answers: PropTypes.shape({
+      default: PropTypes.string.isRequired,
+      textMap: PropTypes.object.isRequired,
+    }),
     questions: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.number.isRequired,
-        question: PropTypes.string.isRequired
-      })
-    ).isRequired
+        question: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
   };
   state = {
     started: false,
@@ -35,9 +31,9 @@ class Kosningaprof extends PureComponent {
     finished: false,
     answers: this.props.questions.reduce((all, { id }) => {
       // eslint-disable-next-line
-      all[id] = defaultAnswer;
+      all[id] = this.props.answers.default;
       return all;
-    }, {})
+    }, {}),
   };
   constructor(props) {
     super(props);
@@ -67,25 +63,24 @@ class Kosningaprof extends PureComponent {
         started: true,
         answers: {
           ...answers,
-          [id]: target.value
-        }
+          [id]: target.value,
+        },
       };
     });
   };
   async onSend() {
-    console.log('this is', this);
     const { answers, token } = this.state;
 
     await this.context.fetch(`/konnun/replies?timestamp=${Date.now()}`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         token,
-        reply: encodeAnswersToken(Object.keys(answers).map(x => answers[x]))
-      })
+        reply: encodeAnswersToken(Object.keys(answers).map(x => answers[x])),
+      }),
     });
 
     this.setState({ finished: true });
@@ -93,6 +88,7 @@ class Kosningaprof extends PureComponent {
   render() {
     const { questions } = this.props;
     const { answers, started, finished } = this.state;
+    const answerMap = this.props.answers.textMap;
     return (
       <div className={s.root}>
         {finished && <h3>Takk fyrir þátttökuna!</h3>}
--- src/routes/prof/index.js
+++ src/routes/prof/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import KosningaProf from './KosningaProf';
 import Layout from '../../components/Layout';
 import questions from '../../../data/poll/questions.json';
+import answers from '../../../data/poll/answers.json';
 
 export default ({ params }) => {
   return {
@@ -10,7 +11,7 @@ export default ({ params }) => {
     path: `/kosningaprof`,
     component: (
       <Layout page="prof" showHeader={false}>
-        <KosningaProf questions={questions} />
+        <KosningaProf answers={answers} questions={questions} />
       </Layout>
     ),
   };
