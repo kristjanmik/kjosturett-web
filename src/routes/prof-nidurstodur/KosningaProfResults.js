e315524789e7704c538b92376d5545c5921eca09
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-22 13:11:40 +0000

    Results stats

--- src/routes/prof-nidurstodur/KosningaProfResults.js
+++ src/routes/prof-nidurstodur/KosningaProfResults.js
@@ -16,39 +16,61 @@ class KosningaprofResults extends PureComponent {
           sunt velit rerum distinctio eius minus quis a nihil repudiandae?
         </p>
         <h3>Stjórnmálaflokkar með svipaðr skoðanir og ég</h3>
-        {parties.map((party, index) => (
-          <Link
-            href={`/flokkur/${party.url}`}
-            className={s.party}
-            key={party.url}
-          >
-            <img
-              src={getAssetUrl('party-icons', party.url)}
-              className={s.partyLogo}
-            />
-            <div className={s.partyName}>{party.name}</div>
-            <div className={s.partyPercentage}>
-              {(100 * (1 - index / parties.length)).toFixed(2)}%
-            </div>
-          </Link>
-        ))}
+        {parties.map((party, index) => {
+          const percent = 100 * (1 - index / parties.length);
+          return (
+            <Link
+              href={`/flokkur/${party.url}`}
+              className={s.party}
+              key={party.url}
+            >
+              <div
+                className={s.partyProgress}
+                style={{
+                  transform: `scaleX(${Math.max(0.01, percent / 100)})`,
+                }}
+              />
+              <img
+                src={getAssetUrl('party-icons', party.url)}
+                className={s.partyLogo}
+              />
+              <div className={s.partyName}>{party.name}</div>
+              <div className={s.partyPercentage}>{percent.toFixed(0)}%</div>
+            </Link>
+          );
+        })}
         <h3>Frambjóðendur með svipaðr skoðanir og ég</h3>
         <div className={s.candidates}>
-          {candidates.slice(0, 12).map((candidate, index) => (
-            <div key={candidate.nafn} className={s.candidate}>
-              <img className={s.candidateImg} src="notfound.jpg" />
-              <div className={s.candidateInfo}>
-                <div className={s.candidateName}>{candidate.nafn}</div>
-                <div className={s.candidateParty}>
-                  {
-                    parties.find(party => party.letter === candidate.bokstafur)
-                      .name
-                  }{' '}
-                  (x{candidate.bokstafur})
+          {candidates.slice(0, 12).map((candidate, index) => {
+            const percent = 100 * (1 - index / 23);
+            return (
+              <div key={candidate.nafn} className={s.candidate}>
+                <img className={s.candidateImg} src="/kristjan.jpg" />
+                <div className={s.candidateProgressBar}>
+                  <div
+                    className={s.candidateProgress}
+                    style={{
+                      transform: `scaleX(${Math.max(0.01, percent / 100)})`,
+                    }}
+                  />
+                </div>
+                <div className={s.candidatePercentage}>
+                  <span>{percent.toFixed(0)}%</span>
+                </div>
+                <div className={s.candidateInfo}>
+                  <div className={s.candidateName}>{candidate.nafn}</div>
+                  <div className={s.candidateParty}>
+                    {
+                      parties.find(
+                        party => party.letter === candidate.bokstafur,
+                      ).name
+                    }{' '}
+                    (x{candidate.bokstafur})
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     );
