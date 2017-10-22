f8f6eb9816a91d6ca2981ac661d1f018ffab8281
Kristjan Mikaelsson <kristjanmik@gmail.com>
2017-10-22 17:56:50 +0000

    fixed replies

--- data/decodeReply.js
+++ data/decodeReply.js
@@ -1,3 +1,3 @@
 const { decodeAnswersToken } = require('../src/utils');
 
-console.log(decodeAnswersToken('6njwioyt:71qfkyyb:181xbxxo0').join(''));
+console.log(decodeAnswersToken('71u1ifac:37yzk5c6:1iaiihxe0').join(''));
