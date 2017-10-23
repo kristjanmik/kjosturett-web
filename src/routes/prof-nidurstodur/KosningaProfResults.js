919b30a587cd9b076ec95dd46619c1369da37db6
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-23 00:34:23 +0000

    Real data

--- src/routes/prof-nidurstodur/KosningaProfResults.js
+++ src/routes/prof-nidurstodur/KosningaProfResults.js
@@ -8,6 +8,7 @@ import { getAssetUrl, candidateImage } from '../../utils';
 class KosningaprofResults extends PureComponent {
   render() {
     const { questions, answers, results, candidates, parties } = this.props;
+    const partyScoreScalar = parties.length ? parties[0].score : 1;
     return (
       <div className={s.root}>
         <h2>Niðurstöður úr kosningaprófi Kjóstu Rétt</h2>
@@ -16,64 +17,63 @@ class KosningaprofResults extends PureComponent {
           sunt velit rerum distinctio eius minus quis a nihil repudiandae?
         </p>
         <h3>Stjórnmálaflokkar með svipaðr skoðanir og ég</h3>
-        {parties.map((party, index) => {
-          const percent = 100 * (1 - index / parties.length);
-          return (
-            <Link
-              href={`/flokkur/${party.url}`}
-              className={s.party}
-              key={party.url}
-            >
-              <div
-                className={s.partyProgress}
-                style={{
-                  transform: `scaleX(${Math.max(0.01, percent / 100)})`,
-                }}
-              />
-              <img
-                src={getAssetUrl('party-icons', party.url)}
-                className={s.partyLogo}
-              />
-              <div className={s.partyName}>{party.name}</div>
-              <div className={s.partyPercentage}>{percent.toFixed(0)}%</div>
-            </Link>
-          );
-        })}
+        {parties.filter(party => party.score).map(party => (
+          <Link
+            href={`/flokkur/${party.url}`}
+            className={s.party}
+            key={party.url}
+          >
+            <div
+              className={s.partyProgress}
+              style={{
+                transform: `scaleX(${Math.max(
+                  0.01,
+                  party.score / partyScoreScalar,
+                )})`,
+              }}
+            />
+            <img
+              src={getAssetUrl('party-icons', party.url)}
+              className={s.partyLogo}
+            />
+            <div className={s.partyName}>{party.name}</div>
+            <div className={s.partyPercentage}>{party.score.toFixed(0)}%</div>
+          </Link>
+        ))}
         <h3>Frambjóðendur með svipaðr skoðanir og ég</h3>
         <div className={s.candidates}>
-          {candidates.slice(0, 12).map((candidate, index) => {
-            const percent = 100 * (1 - index / 23);
-            return (
-              <div key={candidate.nafn} className={s.candidate}>
-                <img
-                  className={s.candidateImg}
-                  src={candidateImage(candidate.slug)}
+          {candidates.slice(0, 12).map(candidate => (
+            <div key={candidate.nafn} className={s.candidate}>
+              <img
+                className={s.candidateImg}
+                src={candidateImage(candidate.slug)}
+              />
+              <div className={s.candidateProgressBar}>
+                <div
+                  className={s.candidateProgress}
+                  style={{
+                    transform: `scaleX(${Math.max(
+                      0.01,
+                      candidate.score / 100,
+                    )})`,
+                  }}
                 />
-                <div className={s.candidateProgressBar}>
-                  <div
-                    className={s.candidateProgress}
-                    style={{
-                      transform: `scaleX(${Math.max(0.01, percent / 100)})`,
-                    }}
-                  />
-                </div>
-                <div className={s.candidatePercentage}>
-                  <span>{percent.toFixed(0)}%</span>
-                </div>
-                <div className={s.candidateInfo}>
-                  <div className={s.candidateName}>{candidate.nafn}</div>
-                  <div className={s.candidateParty}>
-                    {
-                      parties.find(
-                        party => party.letter === candidate.bokstafur,
-                      ).name
-                    }{' '}
-                    (x{candidate.bokstafur})
-                  </div>
+              </div>
+              <div className={s.candidatePercentage}>
+                <span>{candidate.score.toFixed(0)}%</span>
+              </div>
+              <div className={s.candidateInfo}>
+                <div className={s.candidateName}>{candidate.nafn}</div>
+                <div className={s.candidateParty}>
+                  {
+                    parties.find(party => party.letter === candidate.bokstafur)
+                      .name
+                  }{' '}
+                  (x{candidate.bokstafur})
                 </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
     );
--- src/routes/prof-nidurstodur/index.js
+++ src/routes/prof-nidurstodur/index.js
@@ -3,23 +3,30 @@ import KosningaProfResults from './KosningaProfResults';
 import Layout from '../../components/Layout';
 import questions from '../../../data/poll/questions.json';
 import answers from '../../../data/poll/answers.json';
-import candidates from '../../../data/build/candidates.json';
-import parties from '../../../data/build/parties.json';
+import candidateReplies from '../../../data/build/replies-candidates.json';
+import partyReplies from '../../../data/build/replies-parties.json';
+import getResultsByScore from '../../process-replies';
 import { decodeAnswersToken } from '../../utils';
 
-export default ({ params }) => ({
-  chunks: ['prof-nidurstodur'],
-  title: `Kjóstu Rétt - Kosningapróf`,
-  path: `/kosningaprof/:nidurstodur`,
-  component: (
-    <Layout page="prof-nidurstodur">
-      <KosningaProfResults
-        answers={answers}
-        questions={questions}
-        candidates={candidates}
-        parties={parties}
-        results={decodeAnswersToken(params.nidurstodur)}
-      />
-    </Layout>
-  ),
-});
+export default ({ params }) => {
+  const replies = decodeAnswersToken(params.nidurstodur);
+  const parties = getResultsByScore(replies, partyReplies);
+  const candidates = getResultsByScore(replies, candidateReplies);
+
+  return {
+    chunks: ['prof-nidurstodur'],
+    title: `Kjóstu Rétt - Kosningapróf`,
+    path: `/kosningaprof/:nidurstodur`,
+    component: (
+      <Layout page="prof-nidurstodur">
+        <KosningaProfResults
+          answers={answers}
+          questions={questions}
+          candidates={candidates}
+          parties={parties}
+          results={replies}
+        />
+      </Layout>
+    ),
+  };
+};
