1c47ad7c75d364936702c26ebb704523bf5ed631
Kristjan Broder Lund <kristjanlu@novomaticls.com>
2017-10-23 19:27:30 +0000

    kjorskra: Removed autoFocus on home address input

--- src/routes/kjorskra/Kjorskra.js
+++ src/routes/kjorskra/Kjorskra.js
@@ -515,7 +515,6 @@ class Kjorskra extends PureComponent {
                       <Autocomplete
                         ref={this.onAutocompleteMounted}
                         type="text"
-                        autoFocus
                         onChange={this.submitCurrentAddress}
                         placeholder="Núverandi heimilisfang"
                         className={s.input}
