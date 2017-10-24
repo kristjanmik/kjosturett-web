5f78ca6b25844816263d7a69dbd74ca05b8dd082
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-24 16:56:28 +0000

    Add questions compare

--- src/routes/prof-nidurstodur/KosningaProfResults.js
+++ src/routes/prof-nidurstodur/KosningaProfResults.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './styles.scss';
+import { Collapse } from 'react-collapse';
 import Link from '../../Link';
 import { getAssetUrl, candidateImage } from '../../utils';
 
@@ -9,6 +11,17 @@ const scoreToFloatingPoint = (score, scalar = 1) =>
   Math.max(1, Math.ceil(score / scalar)) / 100;
 
 class KosningaprofResults extends PureComponent {
+  state = {
+    open: {},
+  };
+  toggle(party) {
+    this.setState(({ open }) => ({
+      open: {
+        ...open,
+        [party]: !open[party],
+      },
+    }));
+  }
   render() {
     const { questions, answers, results, candidates, parties } = this.props;
     const partyScoreScalar = parties.length ? parties[0].score / 100 : 1;
@@ -31,27 +44,96 @@ class KosningaprofResults extends PureComponent {
           Flokkunum er raðað eftir hversu samála þið eruð.
         </p>
         {parties.filter(party => party.score).map(party => (
-          <Link
-            href={`/flokkur/${party.url}`}
-            className={s.party}
-            key={party.url}
-          >
+          <div>
             <div
-              className={s.partyProgress}
-              style={{
-                transform: `scaleX(${scoreToFloatingPoint(
-                  party.score,
-                  partyScoreScalar,
-                )})`,
+              className={s.party}
+              key={party.url}
+              onClick={() => this.toggle(party.letter)}
+            >
+              <div
+                className={s.partyProgress}
+                style={{
+                  transform: `scaleX(${scoreToFloatingPoint(
+                    party.score,
+                    partyScoreScalar,
+                  )})`,
+                }}
+              />
+              <img
+                src={getAssetUrl('party-icons', party.url)}
+                className={s.partyLogo}
+              />
+              <div className={s.partyName}>{party.name}</div>
+              <div className={s.partyPercentage}>{Math.ceil(party.score)}%</div>
+            </div>
+            <Collapse
+              isOpened={this.state.open[party.letter] === true}
+              springConfig={{
+                stiffness: 100,
+                damping: 20,
               }}
-            />
-            <img
-              src={getAssetUrl('party-icons', party.url)}
-              className={s.partyLogo}
-            />
-            <div className={s.partyName}>{party.name}</div>
-            <div className={s.partyPercentage}>{Math.ceil(party.score)}%</div>
-          </Link>
+            >
+              {questions
+                .map(question => ({
+                  ...question,
+                  myAnswer: question.myAnswer || 3,
+                  partyAnswer: party.reply[question.id] || 3,
+                }))
+                .sort((a, b) => {
+                  const aAgree = Math.abs(a.myAnswer - a.partyAnswer);
+                  const bAgree = Math.abs(b.myAnswer - b.partyAnswer);
+                  if (a.myAnswer === 3 || a.myAnswer === 6) {
+                    return 1;
+                  }
+                  if (
+                    b.myAnswer === 3 ||
+                    b.myAnswer === 6 ||
+                    (isNaN(aAgree) || isNaN(bAgree))
+                  ) {
+                    return -1;
+                  }
+                  return aAgree - bAgree;
+                })
+                .map(({ id, myAnswer, question, partyAnswer }) => {
+                  const indiffrent = !(myAnswer !== 3 && myAnswer !== 6);
+                  const difference = Math.abs(myAnswer - partyAnswer);
+
+                  console.log(difference === 0 && !indiffrent);
+
+                  return (
+                    <div className={s.partyQuestion} key={id}>
+                      <h4>
+                        {!indiffrent && (
+                          <i className={cx(s.dot, s[`dot${difference}`])} />
+                        )}
+                        {question}
+                      </h4>
+
+                      {difference === 0 && !indiffrent ? (
+                        <div>
+                          Við erum{' '}
+                          <strong>
+                            {answers.textMap[myAnswer].toLowerCase()}
+                          </strong>{' '}
+                          þessari staðhæfingu.
+                        </div>
+                      ) : (
+                        <div>
+                          <div>
+                            <strong>{party.name}: </strong>
+                            {answers.textMap[partyAnswer]}
+                          </div>
+                          <div>
+                            <strong>Ég: </strong>
+                            {answers.textMap[myAnswer] || 'Hlutlaus'}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+            </Collapse>
+          </div>
         ))}
         <h3>Frambjóðendur</h3>
         <p className={s.nonLead}>
--- src/routes/prof-nidurstodur/results.js
+++ src/routes/prof-nidurstodur/results.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import KosningaProfResults from './KosningaProfResults';
 import Layout from '../../components/Layout';
-import questions from '../../../data/poll/questions.json';
+import questionsBase from '../../../data/poll/questions.json';
 import answers from '../../../data/poll/answers.json';
 import candidateReplies from '../../../data/build/replies-candidates2.json';
 import partyReplies from '../../../data/build/parties.json';
 import getResultsByScore from '../../process-replies';
 import { decodeAnswersToken } from '../../utils';
 
+function questionAnswer(reply = []) {
+  return reply.reduce((all, answer, index) => {
+    all[index + 1] = parseInt(answer, 10);
+    return all;
+  }, {});
+}
+
 export default ({ params }) => {
   const replies = decodeAnswersToken(params.nidurstodur);
-  const parties = getResultsByScore(replies, partyReplies);
+  const myAnswers = questionAnswer(replies);
+  const parties = getResultsByScore(replies, partyReplies).map(party => {
+    party.reply = questionAnswer((party.reply || '').split(''));
+    return party;
+  });
   const candidates = getResultsByScore(replies, candidateReplies);
+  const questions = questionsBase.map(({ id, question }) => ({
+    id,
+    question,
+    myAnswer: myAnswers[id],
+  }));
 
   return {
     chunks: ['prof-nidurstodur'],
@@ -24,7 +40,6 @@ export default ({ params }) => {
           questions={questions}
           candidates={candidates}
           parties={parties}
-          results={replies}
         />
       </Layout>
     ),
