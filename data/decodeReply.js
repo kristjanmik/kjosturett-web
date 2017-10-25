bbf528aabd905e1358fb23f1256b19f666c4493d
Kristjan Mikaelsson <kristjanmik@gmail.com>
2017-10-25 11:34:52 +0000

    Added dogun reply

--- data/decodeReply.js
+++ data/decodeReply.js
@@ -1,3 +1,3 @@
 const { decodeAnswersToken } = require('../src/utils');
 
-console.log(decodeAnswersToken('6j010jyv:6xllvc3j:1j2hwj4xx').join(''));
+console.log(decodeAnswersToken('6ws0acwp:5n794qmp:ehnnp6qv').join(''));
