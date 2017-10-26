adb71e28d5c64a9ca01faec1a09ba93f4aa6916b
Kristjan Mikaelsson <kristjanmik@gmail.com>
2017-10-26 03:30:48 +0000

    Added hasImage to candidates2.json

--- data/build-candidates2.js
+++ data/build-candidates2.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const globby = require('globby');
+const slugify = require('slugify');
 
 const writeFile = promisify(fs.writeFile);
+const statFile = promisify(fs.stat);
 
 (async () => {
   console.log('Build:Candidates - Building');
@@ -15,6 +17,23 @@ const writeFile = promisify(fs.writeFile);
 
     out = [...out, ...candidates];
   });
+
+  out = await Promise.all(
+    out.map(async candidate => {
+      let hasImage = false;
+      const slug = slugify(candidate.name).toLowerCase();
+      try {
+        const fileInfo = await statFile(`./candidates-images/jpg/${slug}.jpg`);
+        hasImage = fileInfo.isFile();
+      } catch (e) {}
+      console.log(hasImage);
+
+      return {
+        ...candidate,
+        hasImage
+      };
+    })
+  );
 
   await writeFile('./build/candidates2.json', JSON.stringify(out, null, 0));
   console.log('Build:Candidates - Done');
