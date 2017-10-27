aefcb1a9d63e419b6fca477227056e2f4b8146fb
Kristjan Broder Lund <kristjanlu@novomaticls.com>
2017-10-27 20:35:04 +0000

    kjorskra: prevent double submit for address

--- src/routes/kjorskra/Kjorskra.js
+++ src/routes/kjorskra/Kjorskra.js
@@ -519,7 +519,6 @@ class Kjorskra extends PureComponent {
                       <Autocomplete
                         ref={this.onAutocompleteMounted}
                         type="text"
-                        onChange={this.submitCurrentAddress}
                         placeholder="Núverandi heimilisfang"
                         className={s.input}
                       />
