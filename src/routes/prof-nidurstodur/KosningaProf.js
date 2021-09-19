d750f8847df4c66334a054992210a1460a16a862
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2021-09-19 19:38:58 +0000

    fix: navigation ux

--- src/routes/prof-nidurstodur/KosningaProf.js
+++ src/routes/prof-nidurstodur/KosningaProf.js
@@ -150,6 +150,8 @@ class Kosningaprof extends PureComponent {
     const { isEmbedded, questions } = this.props;
     const isLastQuestion = currentQuestionIndex === questions.length - 1;
     const hasAnswer = answers[id] !== null;
+    const hasSomeAnswers = Object.values(answers).some(value => value !== null);
+
     const skipQuestion = () => {
       this.onChange(id)(null);
 
@@ -213,16 +215,27 @@ class Kosningaprof extends PureComponent {
                   className={s.nextPrev}
                   onClick={() => this.changeQuestion(1)}
                   style={{ backgroundColor: 'rgb(34,36,40)' }}
+                  disabled={!hasAnswer}
                 >
                   Áfram
                 </button>
               )}
               {isLastQuestion && (
-                <button className={s.embedSubmit} onClick={() => this.onSend()}>
+                <button
+                  disabled={!hasSomeAnswers}
+                  className={s.embedSubmit}
+                  onClick={() => this.onSend()}
+                >
                   Sjá niðurstöður
                 </button>
               )}
             </div>
+          )}
+          {isLastQuestion && !hasSomeAnswers && (
+            <p style={{ margin: '1rem 0' }}>
+              Til að sjá niðurstöður verður þú að taka afstöðu með eða móti
+              allavega einni fullyrðingu.
+            </p>
           )}
         </div>
       </div>
