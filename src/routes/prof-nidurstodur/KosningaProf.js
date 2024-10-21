3116e906c82222d63f22471bc865a95d80cfc88b
Axel <selii93@gmail.com>
2024-10-21 16:42:40 +0000

    make sure we have string

--- src/routes/prof-nidurstodur/KosningaProf.js
+++ src/routes/prof-nidurstodur/KosningaProf.js
@@ -25,7 +25,7 @@ const marks = {
 };
 
 const isImportant = answer => {
-  return answer && answer.includes('!');
+  return answer && answer.toString().includes('!');
 };
 
 class Kosningaprof extends PureComponent {
