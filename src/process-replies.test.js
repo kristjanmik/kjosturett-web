963889fa26d00bcb82c0cae4c73304e9b0782af4
Axel <selii93@gmail.com>
2024-10-27 13:49:02 +0000

    revert

--- src/process-replies.test.js
+++ src/process-replies.test.js
@@ -1,38 +1,15 @@
-import getResultsBySVTScore from './svt-process-replies';
+import ruvCandidates from '../data/build/replies-candidates.json';
+import getResultsByScore from './process-replies';
 import { decodeAnswersToken } from './utils';
 
 describe('ProcessPoliticsTest: Candiate match ', () => {
-  // random answer
-  const answers = 'MSEsMCwzLDIhLDMsMSEsMCwyLDAhLDEsMiwzIQo=';
-
-  // all 0
-  const zeroAnswer = 'MCwwLDA=';
-
-  const fakeReplies = [{ reply: '3!,0,1,1!,0,0!,1,2,2!,2,3,3!' }];
-  const allThreeReply = [{ reply: '3,3,3' }];
-  const allZeroReply = [{ reply: '0,0,0' }];
+  const answers = '1mm4u75:2d9heev:2jvmtsf';
 
   it('should calculate correct rating', () => {
-    const candidates = getResultsBySVTScore(
+    const candidates = getResultsByScore(
       decodeAnswersToken(answers),
-      fakeReplies
+      ruvCandidates
     );
-    expect(candidates[0].score).toBe(39.42307692307692);
-  });
-
-  it('should calculate 0 rating', () => {
-    const candidates = getResultsBySVTScore(
-      decodeAnswersToken(zeroAnswer),
-      allThreeReply
-    );
-    expect(candidates[0].score).toBe(0);
-  });
-
-  it('should calculate 100 rating', () => {
-    const candidates = getResultsBySVTScore(
-      decodeAnswersToken(zeroAnswer),
-      allZeroReply
-    );
-    expect(candidates[0].score).toBe(100);
+    expect(candidates).toMatchSnapshot();
   });
 });
