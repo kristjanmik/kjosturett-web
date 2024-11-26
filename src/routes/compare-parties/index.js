5e1c9d2aed7f642c9f62a81bde27581b41fbc319
Axel <selii93@gmail.com>
2024-11-26 22:42:28 +0000

    make comparison work for all parties

--- src/routes/compare-parties/index.js
+++ src/routes/compare-parties/index.js
@@ -13,34 +13,7 @@ parties.sort(function(a, b) {
   return 0;
 });
 
-const partyDeflections = {
-  B: 'Framsóknarflokksins',
-  C: 'Viðreisnar',
-  D: 'Sjálfstæðisflokksins',
-  F: 'Flokki fólksins',
-  M: 'Miðflokksins',
-  J: 'Sósíalistaflokksins',
-  P: 'Pírata',
-  S: 'Samfylkingarinnar',
-  O: 'Frjálslynda Lýðræðisflokksins',
-  V: 'Vinstri grænna',
-};
-
-const partyDeflectionsB = {
-  A: 'Bjarta framtíð',
-  B: 'Framsóknarflokkinn',
-  C: 'Viðreisn',
-  D: 'Sjálfstæðisflokkinn',
-  F: 'Flokk fólksins',
-  M: 'Miðflokkinn',
-  J: 'Sósíalistaflokkinn',
-  P: 'Pírata',
-  R: 'Alþýðufylkinguna',
-  S: 'Samfylkinguna',
-  T: 'Dögun',
-  O: 'Frjálslynda Lýðræðisflokkinn',
-  V: 'Vinstri græn',
-};
+const partyLetters = ['B', 'C', 'D', 'F', 'M', 'J', 'P', 'S', 'V', 'Y', 'L'];
 
 const valueMap = {
   0: -1,
@@ -55,7 +28,7 @@ export default ({ url, params }) => {
 
   if (!letters) letters = '';
 
-  letters = letters.split('').filter(letter => !!partyDeflections[letter]);
+  letters = letters.split('').filter(letter => partyLetters.includes(letter));
 
   //Begin calculations
   let filterParties = letters
@@ -90,37 +63,10 @@ export default ({ url, params }) => {
     return Math.abs(max - min);
   });
   const score = Math.min(...replyScore);
-  const percentage = `${score.toFixed(0)}%`;
 
   let title = 'Hversu líkir eru flokkarnir?';
   let description =
     'Nú getur þú borið saman flokkana í ýmsum málum. Hvaða stjórnarmeirihlutar eru líklegir? Prófaðu þig áfram!';
-
-  if (letters.length > 0) {
-    const currentParties = parties.filter(party =>
-      letters.includes(party.letter)
-    );
-
-    if (letters.length === 1) {
-      title = `Berðu ${
-        partyDeflectionsB[currentParties[0].letter]
-      } saman við aðra flokka`;
-      description = `Hversu samstíga eru ${currentParties[0].name} og aðrir flokkar? Þú getu skoðað málið betur hérna.`;
-    } else if (letters.length === 2) {
-      title = `${currentParties
-        .map(p => p.name)
-        .join(' og ')} eru ${percentage} samstíga`;
-      description = `Skoðaðu málið betur ásamt fleiri möguleikum á kjósturétt.is.`;
-    } else {
-      let stjorn = currentParties
-        .map(p => partyDeflections[p.letter])
-        .join(', ');
-      let pos = stjorn.lastIndexOf(', ');
-      stjorn = `${stjorn.substring(0, pos)} og${stjorn.substring(pos + 1)}`;
-      title = `Ríkisstjórnin ${letters.join('')} er ${percentage} samstíga`;
-      description = `Skoðaðu betur stjórn ${stjorn} á kjósturétt.is ásamt fleiri möguleikum á kjósturétt.is.`;
-    }
-  }
 
   return {
     chunks: ['compare-parties'],
